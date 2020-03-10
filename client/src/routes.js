3170a49b70c168312f9477832384cfffa871c9e1
vl <vlyanky17@gmail.com>
2020-03-10 18:15:33 +0300

    make it better

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,5 @@
 import React from "react";
 import Switch from "react-router-dom/es/Switch";
-import Route from "react-router-dom/es/Route";
-
-import Redirect from "react-router-dom/es/Redirect";
-
-import ForAut from "./pages/ForAut";
-import ForTabl from "./pages/ForTabl";
-
-import ForReg from "./pages/ForReg";
 
 
 export const useRoutes = isAuthenticated => {
