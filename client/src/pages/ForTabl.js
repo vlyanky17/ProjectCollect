435975301e585616422dddc65ea7d940adf51a56
vl <vlyanky17@gmail.com>
2020-03-11 01:23:47 +0300

    make it better

--- client/src/pages/ForTabl.js
+++ client/src/pages/ForTabl.js
@@ -79,31 +79,7 @@ console.log(data.userId)
     }
 
 
-    return(<div>
-
-            <nav>
-            <div className="nav-wrapper" style={{ padding: '0 2rem' }}>
-
-    <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a onClick={delet}>Удалить</a></li>
-
-        <li><a onClick={antibanMachine}>Разблокировать</a></li>
-
-        <li><a onClick={banMachine}>Заблокировать</a></li>
-
-        <li><a onClick={registerAll}>Обновить/загрузить таблицу</a></li>
-
-    <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
-    </ul>
-    </div>
-    </nav>
-       <form>
-    < UserTabl users={users}  changeHandler={changeHandler} />
-        </form>
-
-
-    </div>
-        );
+    return(
     }
 
 export default ForTabl;
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,7 +4,7 @@ import Route from "react-router-dom/es/Route";
 import Redirect from "react-router-dom/es/Redirect";
 import ForAut from "./pages/ForAut";
 import ForReg from "./pages/ForReg";
-
+import ForTabl from "./pages/ForTabl";
 
 export const useRoutes = isAuthenticated => {
   
