81793da06148ee884c608673b3e4c882cca09ea4
vl <vlyanky17@gmail.com>
2020-03-10 14:47:44 +0300

    make it better

--- models/user.js
+++ models/user.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {Schema,model} = require('mongoose')
 
-const UserSchema = new Schema({
-    position: String,
-    name: String,
-    company: String,
-    date: Date
-});
+const schema = new Schema({
+    login:{type:String,required:true, unique: true},
+    password: {type:String , required:true},
+    email: {type:String , required:true},
+    datReg: {type:String , required:true},
+    datLog: {type:String , required:true},
+    stat: {type:String , required:true}
+})
 
-const User = mongoose.model('User', UserSchema);
-
-
-module.exports = User;
+module.exports = model('user', schema)
