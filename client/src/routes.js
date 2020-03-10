e97d2c28a9086fffdbe4ca5313c80f9cc5b20bc7
vl <vlyanky17@gmail.com>
2020-03-10 18:18:37 +0300

    make it better

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Switch from "react-router-dom/es/Switch";
-
+import Route from "react-router-dom/es/Route";
+import Redirect from "react-router-dom/es/Redirect";
 
 export const useRoutes = isAuthenticated => {
   
