97006a7f8cad1faeb8adf0b57a9bcebfc6580628
vl <vlyanky17@gmail.com>
2020-03-10 14:58:15 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import 'materialize-css'
+import React, { Component } from 'react';
+import Form from './components/Form';
+import DisplayUsers from './components/DisplayUsers';
+import axios from 'axios';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {useRoutes} from "./routes";
-import {authPage} from "./pages/authPage";
-import {useAuth} from './hooks/auth.hook'
-import {AuthContext} from './context/AuthContext'
+class App extends Component {
+  state = {
+    users: []
+  }
 
-function App() {
+  componentDidMount = () => {
+    this.fetchUsers();
+  };
 
-  const {token, login, logout, userId, ready} = useAuth()
-  const isAuthenticated = !!token
-  const routes = useRoutes(isAuthenticated)
-  return(
-      <AuthContext.Provider value={{
-    token, login, logout, userId, isAuthenticated
-  }}>
-      <Router>
-     <div className="conteiner">
-      {routes}
+  fetchUsers = () => {
+    axios.get('/users')
+      .then((response) => {
+        const { users } = response.data;
+        this.setState({ users: [...this.state.users, ...users] })
+      })
+      .catch(() => alert('Error fetching new users'));
+  };
 
 
-        </div>
-  </Router>
-      </AuthContext.Provider>
-  )
+  addUser = ({ name, position, company }) => {
+    this.setState({
+      users: [...this.state.users, { name, position, company }]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form addUser={this.addUser}/>
+        < DisplayUsers users={this.state.users} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
