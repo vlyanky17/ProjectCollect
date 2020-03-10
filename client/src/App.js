c43de54efc2714e37265f7a27afba7c49c445b43
vl <vlyanky17@gmail.com>
2020-03-10 16:17:50 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Form from './components/Form';
 import DisplayUsers from './components/DisplayUsers';
 import axios from 'axios';
 import './App.css';
-import {useRoutes} from "./routes";
+import {useRoutes} from "./routes/auth.routes";
 import {authPage} from "./pages/authPage";
 import {useAuth} from './hooks/auth.hook'
 import {AuthContext} from './context/AuthContext'
