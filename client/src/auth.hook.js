37d06df071f883d820d53d5ef94d83abb41087f4
vl <vlyanky17@gmail.com>
2020-03-11 22:07:30 +0300

    make it better

--- client/src/auth.hook.js
+++ client/src/auth.hook.js
@@ -5,7 +5,7 @@ import React, { useState,useCallback } from 'react';
 
 export const useAuth = isAuth => {
 
- 
+
 
   const arr =2 ;
   if (isAuth) {  return arr}
--- client/src/pages/ForReg.js
+++ client/src/pages/ForReg.js
@@ -8,39 +8,9 @@ export const ForReg = () =>{
     const {loading, error,request} = useHttp()
     const [form, setForm] = useState( {login:'', password:''})
 
-    const changeHandler = event => {
-        setForm({...form,[event.target.name]: event.target.value})}
 
-    const registerHandler = async () => {
-        try {
-            const data = await request('/api/auth/register', 'POST', {...form})
-
-        } catch (e) {}
-    }
-    return(
-        <div>
-        <form class="form-2">
-        <h1><span class="log-in">Регистрация</span></h1>
-
-    <label for="login"><i class="icon-user"></i>Логин</label>
-    <input type="text" id="login" name="login" placeholder="Логин " onChange={changeHandler} />
-
-    <label for="password"><i class="icon-lock"></i>Пароль</label>
-    <input type="password" id="password" name="password" placeholder="Пароль" class="showpassword"  onChange={changeHandler} />
-
-    <label for="email"><i class="icon-user"></i>email</label>
-    <input type="text" id="email" name="email" placeholder="email " onChange={changeHandler} />
-
-
-    <p className="clearfix">
-
-        <input type="submit" name="submit" value="регистрация" onClick={registerHandler} disabled={loading} href="/"/>
-
-    </p>
-        <a href="/" >Авторизация</a>
-    </form>
-    </div>
-)
+    const arr =2 ;
+    return arr
 
 }
 export default ForReg;
