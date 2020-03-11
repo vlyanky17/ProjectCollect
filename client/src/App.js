2234ba8627fdbec6754e7035c986002dee8efd93
vl <vlyanky17@gmail.com>
2020-03-11 20:21:38 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -11,32 +11,9 @@ import {useRoutes} from './routes'
 import 'materialize-css'
 
 class App extends Component {
-    state = {
-        users: []
-    }
-
-    componentDidMount = () => {
-        this.fetchUsers();
-    };
-
-    fetchUsers = () => {
-        axios.get('/users')
-            .then((response) => {
-                const { users } = response.data;
-                this.setState({ users: [...this.state.users, ...users] })
-            })
-            .catch(() => alert('Error fetching new users'));
-    };
-
-
-    addUser = ({ name, position, company }) => {
-        this.setState({
-            users: [...this.state.users, { name, position, company }]
-        });
-    };
 
     render() {
-      
+
         const routes = useRoutes(false)
 
         return (
