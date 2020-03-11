67671ff52363836cd5fe45e7d4159ecee34264ca
vl <vlyanky17@gmail.com>
2020-03-11 03:14:00 +0300

    make it better

--- client/src/pages/ForTabl.js
+++ client/src/pages/ForTabl.js
@@ -1,9 +1,6 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {NavLink, useHistory} from 'react-router-dom'
-import {AuthContext} from '../context/AuthContext'
-import {useHttp} from "../hooks/http.hook";
 
-import { Checkbox } from 'react-input-checkbox';
 
 
 export const ForTabl = () =>{
