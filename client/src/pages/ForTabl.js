08a46a0813b040fa019adf5befdd7d070e3389da
vl <vlyanky17@gmail.com>
2020-03-11 03:50:46 +0300

    make it better

--- client/src/pages/ForTabl.js
+++ client/src/pages/ForTabl.js
@@ -2,12 +2,16 @@ import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {NavLink, useHistory} from 'react-router-dom'
 import {AuthContext} from '../context/AuthContext'
 import {useHttp} from "../hooks/http.hook";
+import './Aut.css';
 
 
 
 
 
 export const ForTabl = () =>{
+
+
+
     return()
 
 }
