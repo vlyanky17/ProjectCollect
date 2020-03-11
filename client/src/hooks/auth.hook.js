09c4a7ab8d26361113be3a35e3d3eea353bb5f80
vl <vlyanky17@gmail.com>
2020-03-11 19:08:02 +0300

    make it better

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -7,31 +7,7 @@ export const useAuth = () => {
   const [ready, setReady] = useState(false)
   const [userId, setUserId] = useState(null)
 
-  const login = useCallback((jwtToken, id) => {
-    setToken(jwtToken)
-    setUserId(id)
-
-    localStorage.setItem(storageName, JSON.stringify({
-      userId: id, token: jwtToken
-    }))
-  }, [])
 
 
-  const logout = useCallback(() => {
-    setToken(null)
-    setUserId(null)
-    localStorage.removeItem(storageName)
-  }, [])
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem(storageName))
-
-    if (data && data.token) {
-      login(data.token, data.userId)
-    }
-    setReady(true)
-  }, [login])
-
-
-  return {token, login, logout,  userId, ready }
+  return {token,   userId, ready }
 }
