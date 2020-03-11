b3ecdee14ab2a87fa4fa32cf165db292bf67a168
vl <vlyanky17@gmail.com>
2020-03-11 03:27:39 +0300

    make it better

--- client/src/pages/ForTabl.js
+++ client/src/pages/ForTabl.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {NavLink, useHistory} from 'react-router-dom'
 import {AuthContext} from '../context/AuthContext'
+import {useHttp} from "../hooks/http.hook";
 
 
 
