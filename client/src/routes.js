d3bbf00cff52334e21611afc77f35f05581efc50
vl <vlyanky17@gmail.com>
2020-03-11 04:06:39 +0300

    make it better

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,5 +7,31 @@ import ForReg from "./pages/ForReg";
 import ForTabl from "./pages/ForTabl";
 
 export const useRoutes = isAuthenticated => {
-  
+    if (isAuthenticated) {
+        return (
+            <Switch>
+            <Route path="/tabl" exact>
+        <ForTabl/>
+
+        </Route>
+        <Redirect to="/tabl" />
+            </Switch>
+    )
+    }
+
+
+
+    return (
+        <Switch>
+        <Route path="/" exact>
+    <ForAut/>
+
+    </Route>
+    <Route path="/Reg" exact>
+    <ForReg/>
+
+    </Route>
+    <Redirect to="/" />
+        </Switch>
+)
 }
