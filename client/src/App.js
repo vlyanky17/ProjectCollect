cb1b194c52f6091a356cdf255d2d1354d67c1443
vl <vlyanky17@gmail.com>
2020-03-11 19:00:35 +0300

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     };
 
     render() {
-        const {token} = useAuth()
+        const {login} = useAuth()
         const routes = useRoutes(false)
 
         return (
