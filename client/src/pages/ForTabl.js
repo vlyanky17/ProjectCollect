7edcb574986bb6d3ea0b68bec5073d14412fac99
vl <vlyanky17@gmail.com>
2020-03-11 03:41:04 +0300

    make it better

--- client/src/pages/ForTabl.js
+++ client/src/pages/ForTabl.js
@@ -3,7 +3,7 @@ import {NavLink, useHistory} from 'react-router-dom'
 import {AuthContext} from '../context/AuthContext'
 import {useHttp} from "../hooks/http.hook";
 
-
+import { Checkbox } from 'react-input-checkbox';
 
 
 
