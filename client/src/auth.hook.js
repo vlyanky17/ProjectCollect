16244642f041d0b91d18c9aa707b38c4d0d4d276
vl <vlyanky17@gmail.com>
2020-03-11 21:20:42 +0300

    make it better

--- client/src/auth.hook.js
+++ client/src/auth.hook.js
@@ -5,7 +5,7 @@ import React, { useState,useCallback } from 'react';
 
 export const useAuth = isAuth => {
 
-
+  const [token, setToken] = useState(null)
 
   const arr =2 ;
   if (isAuth) {  return arr}
