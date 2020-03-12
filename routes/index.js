9956a4a7e70e0b35dbabeb9ce57f226057eac9fd
vl <vlyanky17@gmail.com>
2020-03-12 03:08:41 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const {check, validationResult} = require('express-validator')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const {Router} = require ('express')
 const router = express.Router();
 
 router.post('/add', async (req, res) => {
