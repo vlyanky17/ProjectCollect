797cf3c5769142cb75b80364e18d1864a78f0f8e
vl <vlyanky17@gmail.com>
2020-03-12 03:48:59 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,9 @@ router.post('/register',
     async(req,res) =>{
         try{
 
-            console.log('1////////////////////////////////////////////////////////')
+            console.log('??????????????????????????????????????????????????r')
             console.log(req.body)
             const errors = validationResult(req)
-
             if (!errors.isEmpty()) { return res.status(400).json({
                 errors:errors.array(),message:'некорректные данные регистрации'
             })}
