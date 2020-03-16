44680f16dd5e46564bbb97db763194df347af00a
vl <vlyanky17@gmail.com>
2020-03-16 07:28:20 +0300

    make it better

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const {Schema,model} = require('mongoose')
 const schema = new Schema({
     login:{type:String,required:true, unique: true},
     password: {type:String , required:true},
-    Admin: {type:String , required:true}
+    Admin: {type:String , required:true},
+    links: [{ type: Types.ObjectId, ref: 'Link' }]
 })
 
 module.exports = model('user', schema)
