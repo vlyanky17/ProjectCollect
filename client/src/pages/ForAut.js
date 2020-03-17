b1a5486aa374d6602d83a404e00b179bda210325
vl <vlyanky17@gmail.com>
2020-03-18 02:59:45 +0300

    make it better

--- client/src/pages/ForAut.js
+++ client/src/pages/ForAut.js
@@ -22,7 +22,7 @@ export const ForAut = () =>{
         try {
             const data = await request('/login', 'POST', {...form})
             auth.login(data.token, data.userId,data.userAdm,data.userlogin)
-if (data) {href="/"}
+
         } catch (e) {}
     }
 
