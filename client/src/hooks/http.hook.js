4305f1d4f06084c61702dbf3c6067803098bf750
vl <vlyanky17@gmail.com>
2020-03-17 05:09:51 +0300

    make it better

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -15,9 +15,9 @@ export const useHttp = () => {
 
 
             const response = await fetch(url, {method, body, headers})
-console.log(response)
-            const data = await response.json()
 
+            const data = await response.json()
+console.log(data)
             if (!response.ok) {
                 throw new Error(data.message || 'Что-то пошло не так')
                 console.log('rec err')
