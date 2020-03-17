a3532b4350c080c726dc556987c1a93be7e1070b
vl <vlyanky17@gmail.com>
2020-03-18 01:06:33 +0300

    make it better

---
+++ models/colec.js
@@ -0,0 +1,27 @@
+const {Schema, model, Types} = require('mongoose')
+
+const schema = new Schema({
+    name: {type: String, required: true},
+    disk: {type: String, required: true},
+    tem: {type: String, required: true},
+    pict: {type: String, required: false},
+    intgr1: {type: String, required: false},
+    intgr2: {type: String, required: false},
+    intgr3: {type: String, required: false},
+    Cstr1: {type: String, required: false},
+    Cstr2: {type: String, required: false},
+    Cstr3: {type: String, required: false},
+    Cdate1: {type: String, required: false},
+    Cdate2: {type: String, required: false},
+    Cdate3: {type: String, required: false},
+    Ctxt1: {type: String, required: false},
+    Ctxt2: {type: String, required: false},
+    Ctxt3: {type: String, required: false},
+    Cbol1: {type: String, required: false},
+    Cbol2: {type: String, required: false},
+    Cbol3: {type: String, required: false},
+    item: [{ type: Types.ObjectId, ref: 'item' }],
+    owner: {type: Types.ObjectId, ref: 'User'}
+})
+
+module.exports = model('colec', schema)
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const schema = new Schema({
     login:{type:String,required:true, unique: true},
     password: {type:String , required:true},
     Admin: {type:String , required:true},
-    item: [{ type: Types.ObjectId, ref: 'item' }]
+    colec: [{ type: Types.ObjectId, ref: 'colec' }]
 
 })
 
