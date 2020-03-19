faf664a16c4554bf3d21e378f27444fe634e8deb
vl <vlyanky17@gmail.com>
2020-03-19 07:26:58 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -7,23 +7,59 @@ import 'materialize-css'
 import ReactDOM from "react-dom";
 import fr from "react-dropzone";
 
-export const ForCol = () =>{
 
 
-    return(  <div>
-        <nav>
-        <div className="nav-wrapper" style={{ padding: '0 2rem' }}>
+class ForCol extends React.Component {
+    constructor(props) {
+        super(props);
 
-<ul id="nav-mobile" className="right hide-on-med-and-down">
+        this.state = {
+            dropzone1: [],
+            dropzone2: []
+        };
+    }
+
+    addFilesToDropzone(files, dropzone) {
+        let files_with_preview = [];
+        files.map(file => {
+            file["preview"] = URL.createObjectURL(file);
+            files_with_preview.push(file);
+        });
+
+        const new_files = [...this.state[dropzone], ...files_with_preview];
+        this.setState({ [dropzone]: new_files });
+    }
+
+    render() {
+        const { dropzone1, dropzone2 } = this.state;
+        return (
+            <div className="App">
+            <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">
+
+            </input>
+            <Dropzone onDrop={files => {this.addFilesToDropzone(files, "dropzone1");}}
+    >
+        {({ getRootProps, getInputProps }) => (
+            <div {...getRootProps()} className="">
+            <input {...getInputProps()} />
+        <div style={{ height: 100, backgroundColor: "yellow" }}>
+            Drop some files here
+            {dropzone1.map(file => (
+                <img
+                src={file.preview}
+                alt={file.path}
+                style={{ width: 40, height: 40 }}
+                />
+            ))}
+        </div>
+        </div>
+        )}
+    </Dropzone>
 
 
-        <li><a  href="/Cab" >назад</a></li>
-    <li><a href="/">Главная </a></li>
-    </ul>
-    </div>
-    </nav>
-
-    </div>
-);
+        </div>
+    );
+    }
 }
+
 export default ForCol;
