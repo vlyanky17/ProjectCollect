3627e3808743657f9305f507d71d0aff1730ee95
vl <vlyanky17@gmail.com>
2020-03-19 06:26:33 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -1,88 +1,27 @@
-
-import React, {useContext, useState} from 'react';
-import './Aut.css';
-import {useHttp} from "../hooks/http.hook";
+import React, {useCallback, useContext, useEffect, useState} from 'react'
+import {NavLink, useHistory} from 'react-router-dom'
 import {AuthContext} from '../context/AuthContext'
+import {useHttp} from "../hooks/http.hook";
 import './Aut.css';
-import ReactDOM from "react-dom";
-import Dropzone from "react-dropzone";
+import 'materialize-css'
 
-class ForCol extends React.Component {
-    constructor(props) {
-        super(props);
+export const ForCol = () =>{
 
-        this.state = {
-            dropzone1: [],
-            dropzone2: []
-        };
-    }
 
-    addFilesToDropzone(files, dropzone) {
-        let files_with_preview = [];
-        files.map(file => {
-            file["preview"] = URL.createObjectURL(file);
-            files_with_preview.push(file);
-        });
+    return(  <div>
+        <nav>
+        <div className="nav-wrapper" style={{ padding: '0 2rem' }}>
 
-        const new_files = [...this.state[dropzone], ...files_with_preview];
-        this.setState({ [dropzone]: new_files });
-    }
+<ul id="nav-mobile" className="right hide-on-med-and-down">
 
-    render() {
-        const { dropzone1, dropzone2 } = this.state;
-        return (
-            <div className="App">
-            <Dropzone
-        onDrop={files => {
-            this.addFilesToDropzone(files, "dropzone1");
-        }}
-    >
-        {({ getRootProps, getInputProps }) => (
-            <div {...getRootProps()} className="">
-            <input {...getInputProps()} />
-        <div style={{ height: 100, backgroundColor: "yellow" }}>
-            Drop some files here
-            {dropzone1.map(file => (
-                <img
-                src={file.preview}
-                alt={file.path}
-                style={{ width: 40, height: 40 }}
-                />
-            ))}
-        </div>
-        </div>
-        )}
-    </Dropzone>
 
-        <div style={{ display: "flex", flexDirection: "row", marginTop: 25 }}>
-    <div style={{ width: "100%" }}>
-        DROPZONE 2
-        <Dropzone
-        onDrop={files => {
-            this.addFilesToDropzone(files, "dropzone2");
-        }}
-    >
-        {({ getRootProps, getInputProps }) => (
-            <div {...getRootProps()} className="">
-            <input {...getInputProps()} />
-        <div style={{ height: 100, backgroundColor: "yellow" }}>
-            Drop some files here
-            {dropzone2.map(file => (
-                <img
-                src={file.preview}
-                alt="dsds"
-                style={{ width: 40, height: 40 }}
-                />
-            ))}
+        <li><a  href="/Cab" >назад</a></li>
+    <li><a href="/">Главная </a></li>
+    </ul>
+    </div>
+    </nav>
+
         </div>
-        </div>
-        )}
-    </Dropzone>
-        </div>
-        </div>
-        </div>
-    );
-    }
+);
 }
-
 export default ForCol;
