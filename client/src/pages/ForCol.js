6e81e1418134a70df58097f46c84cb3be59277c3
vl <vlyanky17@gmail.com>
2020-03-19 08:30:18 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -33,8 +33,7 @@ class ForCol extends React.Component {
     }
 
     render() {
-        const changeHandler = event => {
-            setForm({...form,[event.target.name]: event.target.value})}
+
         const { dropzone1, dropzone2 } = this.state;
         return (
             <div className="App">
