a5e54072e91e0abeab74a20fcfb3130da8eeed8c
vl <vlyanky17@gmail.com>
2020-03-19 09:06:51 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -51,12 +51,7 @@ class ForCol extends React.Component {
         </div>
         </nav>
         <form class="form-2">
-            <label for="name"><i class="icon-user"></i>название</label>
-        <input type="text" id="name" name="name" placeholder="название " onChange={changeHandler} />
-
-        <label for="ops"><i class="icon-user"></i>описание</label>
-        <input type="text" id="ops" name="ops" placeholder="описание " onChange={changeHandler} />
-
+         
 
             <Dropzone onDrop={files => {this.addFilesToDropzone(files, "dropzone1");}}
     >
