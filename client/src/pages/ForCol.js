de97fcae4df39e06cad0ce3ae0aea4c9ee8fdde6
vl <vlyanky17@gmail.com>
2020-03-20 10:23:30 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -67,14 +67,7 @@ const  headers = {}
                     headers['Content-Type'] = 'application/json'
 const url = '/adcol'
 const method= 'POST'
-
-                const response = await fetch(url, {method, body, headers})
-                const data = await response.json()
-
-                if (!response.ok) {
-                    throw new Error(data.message || 'Что-то пошло не так')
-                    console.log('rec err')
-                }
+console.log(body)
 
             } catch (e) {}
         }
