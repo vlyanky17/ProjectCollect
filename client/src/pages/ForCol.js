67f37498bbd47f98f5f7f911e056089da6aa18f1
vl <vlyanky17@gmail.com>
2020-03-21 10:38:41 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -5,10 +5,8 @@ import './Aut.css';
 import './select-css.css';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
 const image2base64 = require('image-to-base64');
 
 
