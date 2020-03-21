7fb49d19350113523f6d1e3f4c6a2b8dc0d366af
vl <vlyanky17@gmail.com>
2020-03-21 07:20:41 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -1,8 +1,13 @@
-import React, { useMemo, useEffect, useState } from "react";
+import React, { useCallback,useContext,useMemo, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { useDropzone } from "react-dropzone";
 const image2base64 = require('image-to-base64');
+import {AuthContext} from '../context/AuthContext'
+import {useHttp} from "../hooks/http.hook";
 
+
+
+import 'materialize-css'
 
 const baseStyle = {
     flex: 1,
@@ -163,6 +168,17 @@ function ForCol(props) {
 
     return (
         <div>
+            <nav>
+                <div className="nav-wrapper" style={{ padding: '0 2rem' }}>
+
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+
+                        <li><a >Добро пожаловать {us}</a></li>
+                        <li><a  href="/Cab">Назад</a></li>
+                        <li><a href="/">Главная </a></li>
+                    </ul>
+                </div>
+            </nav>
             <div className='container' onChange={changeH}>
                 <div {...getRootProps({ style })}>
                     <input {...getInputProps()} />
