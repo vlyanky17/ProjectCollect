eb3fcaf8f6cb839c46de6461fe054db1d34b710c
vl <vlyanky17@gmail.com>
2020-03-21 10:21:07 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -225,6 +225,41 @@ function ForCol(props) {
                                 </ul>
                             </section>
                     </div>
+                    <style>
+                        .tab input, .tab-content { display: none; }
+                        .tab {
+                        font: 0.8rem/1.2 Arial, sans-serif; /* Параметры шрифта */
+                        border: 1px solid #e9eaec; /* Параметры рамки */
+                        border-radius: 3px; /* Скругляем уголки */
+                        color: #848994; /* Цвет текста */
+                        margin-bottom: 10px; /* Расстояние между пунктами */
+                    }
+                        .tab-title {
+                        padding: 10px; /* Поля вокруг текста */
+                        display: block; /* Блочный элемент */
+                        text-transform: uppercase; /* Все буквы заглавные */
+                        font-weight: bold; /* Жирное начертание */
+                        cursor: pointer; /* Вид курсора */
+                    }
+                        .tab-title::after {
+                        content: '+'; /* Выводим плюс */
+                        float: right; /* Размещаем по правому краю */
+                    }
+                        .tab-content {
+                        padding: 10px 20px; /* Поля вокруг текста */
+                    }
+                        .tab :checked + .tab-title {
+                        background-color: #50a2de; /* Цвет фона */
+                        border-radius: 3px 3px 0 0; /* Скругляем уголки */
+                        color: #fff; /* Цвет текста */
+                    }
+                        .tab :checked + .tab-title::after {
+                        content: '−'; /* Выводим минус */
+                    }
+                        .tab :checked ~ .tab-content {
+                        display: block; /* Показываем содержимое */
+                    }
+                    </style>
                     <div className="tab">
                         <input type="checkbox" id="tab2" name="tab-group"/>
                             <label htmlFor="tab2" className="tab-title">Можно ли программировать на HTML5?</label>
