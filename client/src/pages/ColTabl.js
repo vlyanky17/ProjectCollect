6baf225acd958dcf19ab1dbee1d4a171ece1530d
vl <vlyanky17@gmail.com>
2020-03-22 18:49:27 +0300

    can

--- client/src/pages/ColTabl.js
+++ client/src/pages/ColTabl.js
@@ -3,6 +3,7 @@ import './Aut.css';
 import {useHttp} from "../hooks/http.hook";
 import {AuthContext} from '../context/AuthContext'
 import {NavLink, useHistory} from 'react-router-dom'
+import ColMap from "../component/ColMap";
 
 export const ColTabl = () =>{
     const {loading, error,request} = useHttp()
