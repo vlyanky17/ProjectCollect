0df9588da353450f639375148824ae1ce7b0ff07
vl <vlyanky17@gmail.com>
2020-03-22 18:23:40 +0300

    can

--- client/src/pages/ForCab.js
+++ client/src/pages/ForCab.js
@@ -2,7 +2,7 @@ import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {NavLink, useHistory} from 'react-router-dom'
 import {AuthContext} from '../context/AuthContext'
 import {useHttp} from "../hooks/http.hook";
-
+import ColTabl from "../pages/ColTabl";
 
 
 import 'materialize-css'
