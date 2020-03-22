18bcad203eaccf67c13bf9b81ceb37c7c3b86fed
vl <vlyanky17@gmail.com>
2020-03-22 06:45:17 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -71,7 +71,7 @@ const img = {
 
 
 
-function ForCol(props) {
+export const ForCol = () =>{
     const [form, setForm] = useState( {nam:'', disk:'',tem:'',pict:'',tem:'',intgr1:'',intgr2:'',intgr3:'',Cstr1:'',Cstr2:'',Cstr3:'',Cdate1:'',Cdate2:'',Cdate3:'',Ctxt1:'',Ctxt2:'',Ctxt3:'',Cbol1:'',Cbol2:'',Cbol3:''})
     const [yo, setyo] = useState( '')
     const [base6, setbase6] = useState( '')
