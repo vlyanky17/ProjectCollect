6f980210664abf37439c80d8f576bf0e20eb99ec
vl <vlyanky17@gmail.com>
2020-03-22 14:38:44 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -262,7 +262,7 @@ router.post('/adcol',auth, async(req,res) =>{
         const {nam,disk,tem,pict,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3} = req.body
         console.log(nam)
         const colec = new Colec({
-            nam,disk,tem,pict,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3, owner: req.user.userId
+            nam:req.body.nam,  disk:req.body.disk, tem:req.body.tem, owner: req.user.userId
         })
         console.log(colec)
 
