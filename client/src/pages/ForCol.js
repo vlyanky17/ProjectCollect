dc80e46e5ab330243fcb643c5a3687a218fecc31
vl <vlyanky17@gmail.com>
2020-03-22 08:46:05 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -158,7 +158,7 @@ export const ForCol = () =>{
                     //cGF0aC90by9maWxlLmpwZw==
                     console.log('111111')
                     setForm({...form,["pict"]:response})
-                    console.log(response)
+                    console.log(form.pict)
                 }
             )
             .catch(
@@ -233,7 +233,7 @@ export const ForCol = () =>{
 
 
             </div>
-            <img src={'data:image/jpeg;base64,'} style={{ width: 200, height: 200 }} />
+            <img src={'data:image/jpeg;base64,'form.pict} style={{ width: 200, height: 200 }} />
 
                 <label htmlFor="intgr1"><i className="icon-user"></i>первое числовое поле</label>
                 <input type="text" id="intgr1" name="intgr1" placeholder="название числового поля "
