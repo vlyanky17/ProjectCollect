f5f8ce22019892ac587918ea3d8f38c59a6f3ca7
vl <vlyanky17@gmail.com>
2020-03-22 06:12:42 +0300

    make it better

--- client/src/pages/ForCol.js
+++ client/src/pages/ForCol.js
@@ -114,13 +114,13 @@ function Tabl(props) {
         setForm({...form,[event.target.name]: event.target.value})
     console.log(event.target.name)
         console.log(event.target.value)
-        setForm({...form,["pict"]:yo})
+
     }
 
     const addCol = async () => {
         try {
 
-
+            setForm({...form,["pict"]:yo})
          //   const data = await request('/adcol', 'POST', {...form})
 
         } catch (e) {}
