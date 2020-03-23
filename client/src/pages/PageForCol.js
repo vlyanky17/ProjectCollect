4f8f0bd01fa18f22e73bf7cb3e50c6139161ba6e
vl <vlyanky17@gmail.com>
2020-03-23 10:03:39 +0300

    can

--- client/src/pages/PageForCol.js
+++ client/src/pages/PageForCol.js
@@ -14,7 +14,7 @@ export const PageForCol = () =>{
     const ut = data.logn
     const ur = data.ColId
     return( <div>
-        {ur}  {ut}
+        {ur}
         1111111112222221111
 
     </div>
