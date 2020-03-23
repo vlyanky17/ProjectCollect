b179c3f15c887213f302592a98ef5e7b5aacf3b5
vl <vlyanky17@gmail.com>
2020-03-23 10:50:43 +0300

    can

--- client/src/pages/PageForCol.js
+++ client/src/pages/PageForCol.js
@@ -13,6 +13,18 @@ export const PageForCol = () =>{
     const data = JSON.parse(localStorage.getItem(storageName))
 const us = data.ColId
     return( <div>
+        <nav>
+        <div className="nav-wrapper" style={{ padding: '0 ' }}>
+
+<ul id="nav-mobile" className="right hide-on-med-and-down">
+
+
+        <li><a  href="/Cab" >назад</a></li>
+    <li><a href="/">Главная </a></li>
+    </ul>
+    </div>
+    </nav>
+
         {us}
 
 
