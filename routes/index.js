896788fac63cec8a4fac9940f3ebd723fa0601e3
vl <vlyanky17@gmail.com>
2020-03-23 12:33:47 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -292,7 +292,7 @@ router.post('/ColUp', async(req,res) =>{
     try{
         console.log(req.body)
         const coleks = await Colec.find({ _id: req.body.us })
-
+        console.log(coleks)
     } catch(e){
         res.status(500).json({message: 'error Col'})
     }
