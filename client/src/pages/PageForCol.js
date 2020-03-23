270dd1f8eb1c4fdd6249919fe46db5c41d3b90c4
vl <vlyanky17@gmail.com>
2020-03-23 13:42:41 +0300

    make it better

--- client/src/pages/PageForCol.js
+++ client/src/pages/PageForCol.js
@@ -11,7 +11,7 @@ import 'materialize-css'
 export const PageForCol = () =>{
     const {loading, error,request} = useHttp()
     const history = useHistory()
-    const [form, setForm] = useState( undefined)
+    const [form, setForm] = useState( null)
     const storageName = 'userData'
     const data = JSON.parse(localStorage.getItem(storageName))
 const us = data.ColId
@@ -27,7 +27,7 @@ console.log(data)
     }
 
     useEffect(() => {
-        if (form ==undefined) {registerAll()
+        if (form ==null) {registerAll()
             console.log("usEf")
         }
 
@@ -58,9 +58,9 @@ console.log(data)
         </tr>
         </thead>
         <tbody>
-        <td>{form.nam}</td>
-        <td>{form.disk}</td>
-        <td>{form.tem}</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
         </tbody>
     </table>
 
