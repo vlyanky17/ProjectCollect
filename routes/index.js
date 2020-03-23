5629c4867d56082c2157e0510b08963df033859d
vl <vlyanky17@gmail.com>
2020-03-23 04:54:06 +0300

    can

--- routes/index.js
+++ routes/index.js
@@ -277,9 +277,7 @@ router.post('/ToCabCol', async(req,res) =>{
     try{
 console.log(req.body)
         const Firstcolecs = await Colec.find({ owner: req.body.us })
-        console.log(Firstcolecs)
-   const SecColec = {Firstcolecs.nam,Firstcolecs.disk,Firstcolecs.tem }
-   console.log(SecColec)
+   console.log(Firstcolecs)
     } catch(e){
         res.status(500).json({message: 'error Col'})
     }
