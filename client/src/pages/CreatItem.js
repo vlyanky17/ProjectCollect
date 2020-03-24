832b232680b5f0667c53b3e1ff0772e0c1ab6bae
vl <vlyanky17@gmail.com>
2020-03-24 11:04:47 +0300

    make it better

--- client/src/pages/CreatItem.js
+++ client/src/pages/CreatItem.js
@@ -38,7 +38,7 @@ export const CreatItem = () =>{
         }
     )
 
-    if (col.intgr1=='') {var intgr1=  <input type="text" id="intgr1" name="intgr1" placeholder="название " onChange={changeHandler} />}
+    if (col.intgr1!='') {var intgr1=  <input type="text" id="intgr1" name="intgr1" placeholder=col.intgr1  onChange={changeHandler} />}
     return (
         <div>
         <nav>
