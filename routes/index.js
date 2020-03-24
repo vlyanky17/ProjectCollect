e218b57d6672c1a8b00c1957d2370ee2d322946e
vl <vlyanky17@gmail.com>
2020-03-24 15:07:45 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -305,13 +305,13 @@ router.post('/ToItm', async(req,res) =>{
 
     try{
         console.log(req.body)
-        const {nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3} = req.body
+        const {nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3,id} = req.body
         req.body.tags.forEach(function(item, i, arr) {
             console.log(item)
             console.log(i)
 
         });
-
+        console.log(nazv+' '+intgr1+' '+intgr2+' '+intgr3+' '+Cstr1+' '+Cstr2+' '+Cstr3+' '+Cdate1+' '+Cdate2+' '+Cdate3+' '+Ctxt1+' '+Ctxt2+' '+Ctxt3+' '+Cbol1+' '+Cbol2+' '+Cbol3+' '+id)
     } catch(e){
         res.status(500).json({message: 'error Col'})
     }
