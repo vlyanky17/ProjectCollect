92b1daa746156d001cb645d20ad3b9ededcb3f1c
vl <vlyanky17@gmail.com>
2020-03-24 20:24:00 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -306,7 +306,7 @@ router.post('/ColUp', async(req,res) =>{
 router.post('/ToItm', async(req,res) =>{
 
     try{
-let y =0;
+
         const {nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3,id} = req.body
         req.body.tags.forEach(function(item, i, arr) {
 
@@ -318,25 +318,8 @@ let y =0;
 
 
 
-let i=0;
-        console.log("whi0")
 
-            try {
-                console.log("whi1")
-                i++
-                console.log(i)
-                console.log("whi2")
-                const  itm =  req.body.tags[i]
-                console.log("whi3")
-                const tag = new Tag({  nam: itm, owner: item._id})
-                console.log("whi4")
-                console.log(tag)
-            }catch (e) {
 
-                break
-            }
-
-        console.log("whiOv")
     } catch(e){
         res.status(500).json({message: 'error Col'})
     }
