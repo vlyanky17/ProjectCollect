c826e53c3f59ac821e2f6fa28b2f94b4d4624484
vl <vlyanky17@gmail.com>
2020-03-24 20:55:04 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -312,7 +312,7 @@ router.post('/ToItm', async(req,res) =>{
 
         const item = new Item({nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3, owner: id})
         console.log(item)
-
+        await item.save()
 
 
         for (var i = 1; i < req.body.tags.length; i++) {
@@ -320,6 +320,7 @@ router.post('/ToItm', async(req,res) =>{
             const na = '#'+req.body.tags[i]
 const tag = new Tag({nam:na  ,owner: item._id})
             console.log(tag)
+            await tag.save()
         }
 
     } catch(e){
