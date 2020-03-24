ca33109d2fe34b758e7240d1374014c00c2d5cf7
vl <vlyanky17@gmail.com>
2020-03-24 05:36:02 +0300

    can it

--- client/src/component/ColMap.js
+++ client/src/component/ColMap.js
@@ -38,7 +38,7 @@ export const ColMap = ({ cols }) => {
     { cols.map((col, index) => {
             return (
 
-                <tr key={col.disk}>
+                <tr key={col.nam}>
 
 
                 <td>{col.nam}</td>
