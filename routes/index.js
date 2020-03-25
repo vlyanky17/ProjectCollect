bcbbb6ca09f9974ef669676b5d1d94ae12a2f36d
vl <vlyanky17@gmail.com>
2020-03-25 09:38:22 +0300

    can it

--- routes/index.js
+++ routes/index.js
@@ -310,7 +310,7 @@ router.post('/ToItm', async(req,res) =>{
         const {nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3,id} = req.body
 
 
-        const item = new Item({nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3})
+        const item = new Item({nazv,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3, owner: id})
         console.log(item)
         await item.save()
 
