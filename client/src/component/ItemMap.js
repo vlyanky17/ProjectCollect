e879600047563fffc2ca3a7b3dbcaad9f7bd59c9
vl <vlyanky17@gmail.com>
2020-03-25 13:53:26 +0300

    can it

--- client/src/component/ItemMap.js
+++ client/src/component/ItemMap.js
@@ -31,7 +31,7 @@ export const ItemMap = ({ itms }) => {
         return <p className="center">нет айтемов</p>
     }
 
-    const text = <div>текст</div>;
+    const tp = <div>    <th>название</th></div>;
     return (
         <table class="resp-tab">
         <thead>
