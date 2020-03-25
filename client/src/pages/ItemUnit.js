0bbefa056d69dfa1e86d813c1c576f61d579a28a
vl <vlyanky17@gmail.com>
2020-03-25 18:56:04 +0300

    can it

--- client/src/pages/ItemUnit.js
+++ client/src/pages/ItemUnit.js
@@ -10,9 +10,29 @@ import 'materialize-css'
 
 
 export const ItemUnit = () => {
+    const {loading, error,request} = useHttp()
+    const [itm, setitm] = useState({nam:''})
     const storageName = 'userData'
     const data = JSON.parse(localStorage.getItem(storageName))
     const us = data.ItId
+
+    const registerAl = async () => {
+        try {
+            console.log(us)
+            const dat = await request('/GetItm', 'POST',{us})
+
+            setitm(dat[0])
+            console.log(dat[0])
+
+        } catch (e) {}
+    }
+
+    useEffect(() => {
+        if (itm.nam==''){console.log('lod')
+            registerAl()}
+        console.log(itm)
+        console.log("1111111") }
+    )
 
     return( <div>
         {us}
