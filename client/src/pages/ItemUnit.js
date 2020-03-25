71749fe36ec1c02efbc9fad3b6cce94e2e31e582
vl <vlyanky17@gmail.com>
2020-03-25 19:48:39 +0300

    can it

--- client/src/pages/ItemUnit.js
+++ client/src/pages/ItemUnit.js
@@ -12,6 +12,7 @@ import 'materialize-css'
 export const ItemUnit = () => {
     const {loading, error,request} = useHttp()
     const [itm, setitm] = useState({nam:''})
+    const [tag, settag] = useState({nam:''})
     const storageName = 'userData'
     const data = JSON.parse(localStorage.getItem(storageName))
     const us = data.ItId
@@ -22,6 +23,8 @@ export const ItemUnit = () => {
             const dat = await request('/GetItm', 'POST',{us})
 
             setitm(dat[0])
+            delete dat[0];
+            settag(dat)
             console.log(dat)
 
         } catch (e) {}
