16d286f6e1ecbba2d9677e0bf0cef3b8b55028df
vl <vlyanky17@gmail.com>
2020-03-25 10:48:05 +0300

    can it

--- client/src/pages/ItemTabl.js
+++ client/src/pages/ItemTabl.js
@@ -3,6 +3,7 @@ import './Aut.css';
 import {useHttp} from "../hooks/http.hook";
 import {AuthContext} from '../context/AuthContext'
 import {NavLink, useHistory} from 'react-router-dom'
+import ItemMap from "../component/ItemMap";
 
 
 export const ItemTabl = () =>{
