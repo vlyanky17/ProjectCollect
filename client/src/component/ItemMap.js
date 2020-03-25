44ee34216d219430743b3198e62225bb4baeb1dc
vl <vlyanky17@gmail.com>
2020-03-25 10:56:31 +0300

    can it

--- client/src/component/ItemMap.js
+++ client/src/component/ItemMap.js
@@ -4,38 +4,16 @@ import "../pages/Aut.css";
 
 
 export const ItemMap = ({ itms }) => {
-    const sortBy = require('lodash/sortBy');
+
     if (!itms.length) {
         return <p className="center">нет коллекций</p>
     }
-    const changeHandler = event => {
-        console.log(event.target.name)
-        const storageName = 'userData'
-        const data = JSON.parse(localStorage.getItem(storageName))
-        localStorage.setItem(storageName, JSON.stringify({
-            userId: data.userId, token: data.token,Adm: data.Adm,logn:data.logn,ColId: data.ColId
 
-        }))
-
-    }
 
 
 
     return (
-        <table class="resp-tab">
-        <thead>
-        <tr>
-
-        <th>Название</th>
-        <th>теги</th>
-
-        <th> </th>
-
-        </tr>
-        </thead>
-
-
-    </table>
+     <div>111111 <div>
 
 )
 }
