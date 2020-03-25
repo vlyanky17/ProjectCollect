178c3e9df5b2c18db16775dc7f941386f667e869
vl <vlyanky17@gmail.com>
2020-03-25 11:42:14 +0300

    can it

--- client/src/component/ItemMap.js
+++ client/src/component/ItemMap.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import "../pages/Aut.css";
+import {useHttp} from "../hooks/http.hook";
 
 
 export const ItemMap = ({ itms }) => {
-
+    const {loading, error,request} = useHttp()
+    const [lod, setLod] = useState(null)
+    const storageName = 'userData'
+    const data = JSON.parse(localStorage.getItem(storageName))
+    const us = data.ColId
     if (!itms.length) {
         return <p className="center">нет айтемов</p>
+    }
+    useEffect(() => {
+       if (lod ==null){
+
+       }
+        }
+    )
+    const registerAll = async () => {
+        try {
+            console.log(us)
+            const data = await request('/ToCabCol', 'POST',{us})
+            setitms(data)
+            console.log(data)
+        } catch (e) {}
     }
 
 
