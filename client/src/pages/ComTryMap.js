4cc07dcf0ca125bae40ae3700c309011adccaf72
vl <vlyanky17@gmail.com>
2020-03-26 10:58:14 +0300

    make it better

---
+++ client/src/pages/ComTryMap.js
@@ -0,0 +1,12 @@
+import React, {useCallback, useContext, useEffect, useState} from 'react'
+
+
+
+export const ComTryMap = () => {
+    return(<div>
+        1111111111111
+        </div>
+)
+}
+
+export default ComTryMap;
--- client/src/pages/LikesComent.js
+++ client/src/pages/LikesComent.js
@@ -2,7 +2,7 @@ import React, {useCallback, useContext, useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Aut.css';
 import {useHttp} from "../hooks/http.hook";
-import ColMap from "../component/ColMap";
+import ComTryMap from "./ComTryMap";
 
 
 
@@ -66,7 +66,7 @@ console.log(d)
 
         </table>
 
-
+<ComTryMap />
         </div>
 )
 }
