4dbce4a8520ec92505787c76e87d3d3ee2cdf764
vl <vlyanky17@gmail.com>
2020-03-26 14:44:25 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -262,12 +262,17 @@ router.get('/users', async (req, res) => {
 });
 
 
-router.post('/adcol',auth, async(req,res) =>{
+router.post('/adcol',[check('nam','введите название').exists(),check('disk','введите описание').exists(),check('tem','введите тему').exists() ],auth, async(req,res) =>{
 
     try{
+        const errors = validationResult(req.body)
+        if (!errors.isEmpty()) { return res.status(400).json({
+            errors:errors.array(),message:'некорректные данные'
+        })}
 
         const {nam,disk,tem,pict,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3} = req.body
         console.log(req.user.userId)
+
         const colec = new Colec({nam,disk,tem,pict,intgr1,intgr2,intgr3,Cstr1,Cstr2,Cstr3,Cdate1,Cdate2,Cdate3,Ctxt1,Ctxt2,Ctxt3,Cbol1,Cbol2,Cbol3, owner: req.user.userId})
 
         await colec.save()
