231d2e17fe23a6828698c657b5d6777d0918275e
vl <vlyanky17@gmail.com>
2020-03-26 08:27:17 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -383,7 +383,7 @@ router.post('/InCom', async(req,res) =>{
         console.log("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 console.log(req.body.com)
         const d = new Date();
-const coment = new Coment(nam:req.body.com.nam )
+const coment = new Coment(nam:"111" )
 
     } catch(e){
         res.status(500).json({message: 'error Col'})
