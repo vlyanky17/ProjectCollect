94a5c52f8eab9be85241d01633da6a2be1776136
vl <vlyanky17@gmail.com>
2020-03-26 12:51:35 +0300

    make it better

--- client/src/pages/LikesComent.js
+++ client/src/pages/LikesComent.js
@@ -67,7 +67,7 @@ console.log(d)
 
         <th> <form class="form-2"> комментарии   <input type="text" id="comen" name="comen" placeholder="комментарий " onChange={changeHandler} />
     <button onClick={ToComent}>  отправить</button>  </form> </th>
-        <th><button  id="Ok"  onClick={ToLike}><img src={logo} alt="logo" /> </button></th>
+        <th> <form class="form-2"> <button  onClick={ToLike}><img src={logo} alt="logo" /> </button> </form> </th>
         </tr>
         </thead>
     <tbody>
