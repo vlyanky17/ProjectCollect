9edfd51f19ea2dd5c86dee55b8351d831d31d616
vl <vlyanky17@gmail.com>
2020-03-26 08:14:32 +0300

    make it better

--- models/Coment.js
+++ models/Coment.js
@@ -3,7 +3,7 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
     nam: {type: String, required: true},
     comen: {type: String, required: true},
-    dat: {type: Date, required: true},
+    dat: {type: String, required: true},
     owner: {type: Types.ObjectId, ref: 'Item'}
 })
 
--- routes/index.js
+++ routes/index.js
@@ -382,7 +382,9 @@ router.post('/InCom', async(req,res) =>{
     try{
         console.log("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 console.log(req.body.com)
-        console.log("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+        const d = new Date();
+     const comen = Coment(nam:req.body.com.nam , comen:req.body.com.comen ,dat:d.toDateString() ,owner:req.body.com.id)
+        console.log(comen)
 
     } catch(e){
         res.status(500).json({message: 'error Col'})
