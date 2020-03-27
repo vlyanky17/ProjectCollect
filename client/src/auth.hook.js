a7f6bebe2ea8d1700f54fce14b87f209c860e820
vl <vlyanky17@gmail.com>
2020-03-27 10:19:09 +0300

    make it better

--- client/src/auth.hook.js
+++ client/src/auth.hook.js
@@ -8,22 +8,25 @@ export const useAuth = isAuth => {
   const [ready, setReady] = useState(false)
   const [userId, setUserId] = useState(null)
   const [reservuserId, setreservUserId] = useState(null)
+  const [reservLog, setreservLog] = useState(null)
   const [userAdm, setUserAdm] = useState(null)
   const [userLog, setUserLog] = useState(null)
   const [ColId, setColId] = useState(null)
   const [ItId, setItId] = useState(null)
-  const login = useCallback((jwtToken, id,Adm,logn,RID,Col,Itd) => {
+  const login = useCallback((jwtToken, id,Adm,logn,RID,RLog,Col,Itd) => {
     setToken(jwtToken)
     setUserId(id)
     setUserAdm(Adm)
     setUserLog(logn)
     setreservUserId(RID)
+    reservLog(RLog)
 console.log(id)
     console.log(Adm)
     console.log(logn)
     console.log(RID)
+    console.log(RLog)
     localStorage.setItem(storageName, JSON.stringify({
-      userId: id, token: jwtToken,Adm:Adm,logn:logn,ColId:Col,ItId:Itd,ResId:RID
+      userId: id, token: jwtToken,Adm:Adm,logn:logn,ColId:Col,ItId:Itd,ResId:RID,ResLog:RLog
     }))
   }, [])
 
@@ -36,6 +39,7 @@ console.log(id)
     setColId(null)
     setItId(null)
     setreservUserId(null)
+    setreservLog(null)
     localStorage.removeItem(storageName)
   }, [])
 
@@ -44,12 +48,12 @@ console.log(id)
 
     if (data && data.token) {
 
-      login(data.token, data.userId, data.Adm, data.logn,data.ResId,data.ColId,data.ItId)
+      login(data.token, data.userId, data.Adm, data.logn,data.ResId,data.ResLog,data.ColId,data.ItId)
     }
     setReady(true)
   }, [login])
 
 
-  return { login, logout, token, userId, ready, userAdm,userLog,ColId,ItId,reservuserId}
+  return { login, logout, token, userId, ready, userAdm,userLog,ColId,ItId,reservuserId,reservLog}
 
 }
