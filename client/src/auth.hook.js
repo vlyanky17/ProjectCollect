1b93742156dcd16c046d3beec7d41f1d147af27b
vl <vlyanky17@gmail.com>
2020-03-27 11:04:11 +0300

    make it better

--- client/src/auth.hook.js
+++ client/src/auth.hook.js
@@ -19,12 +19,13 @@ export const useAuth = isAuth => {
     setUserAdm(Adm)
     setUserLog(logn)
     setreservUserId(RID)
+    setreservLog(Rlog)
     console.log('|||||||||||||||||||||||')
-console.log(id)
-    console.log(Adm)
-    console.log(logn)
-    console.log(RID)
-    console.log(Rlog)
+console.log(userId)
+    console.log(userAdm)
+    console.log(userLog)
+    console.log(reservuserId)
+    console.log(reservLog)
     console.log('|||||||||||||||||||||||')
     localStorage.setItem(storageName, JSON.stringify({
       userId: id, token: jwtToken,Adm:Adm,logn:logn,ColId:Col,ItId:Itd,ResId:RID
