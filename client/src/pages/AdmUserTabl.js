a8415a7b037f062dae667fc8bd0f8cc956d33c77
vl <vlyanky17@gmail.com>
2020-03-27 11:59:11 +0300

    make it better

--- client/src/pages/AdmUserTabl.js
+++ client/src/pages/AdmUserTabl.js
@@ -39,10 +39,7 @@ console.log(usrs[0])
 
     return( <div>
 
-        {us} ||||||||||
-    {ResI} ||||||||||||||
-    {log} ||||||||||||
-    {ResL} ||||||||
+       
 <UserMap usrs={usrs} />
     </div>
 );
