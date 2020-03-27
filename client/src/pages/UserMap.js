e189425e0f90df000641f0015aab8e12e0f7c073
vl <vlyanky17@gmail.com>
2020-03-27 08:31:10 +0300

    make it better

--- client/src/pages/UserMap.js
+++ client/src/pages/UserMap.js
@@ -1,7 +1,9 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react'
 
 export const UserMap = ({ usrs }) => {
-
+    if (!usrs.length) {
+        return <p className="center">нет коментариев</p>
+    }
 
     return (
         <div>
