dd0d29c07eb6d0efcf116920ea0c935259ec04f9
vl <vlyanky17@gmail.com>
2020-03-28 13:09:55 +0300

    make it better

--- client/src/pages/TagMap.js
+++ client/src/pages/TagMap.js
@@ -13,13 +13,21 @@ export const TagMap = ({ tags }) =>{
     }
 
 
-    return (  { tags.map((usr, index) => {
-    
-            return (<div>
-                {usr}
-                </div>
+    return (  <div>
+        { tags.map((usr, index) => {
+
+            return (
+                <tr key={usr._id}>
+                <td> {usr.login}</td>
+                <td>  {IAmd}</td>
+                <td> {usr.Ban}</td>
+                <td><a  href="/Cab">    <form class="form-2"><button name={usr._id} id={usr.login}  onClick={changeHandler} >кабинет</button></form></a> </td>
+            <td><a   >     <form class="form-2"><button name={usr._id}  onClick={BanMachine} > заблокировать/разблокироввать </button>  </form></a> </td>
+            <td><a  >     <form class="form-2"><button name={usr._id}  onClick={Deleter} > Удалить</button>  </form> </a> </td>
+            </tr>
         )
-        }) })
+        }) }
+        </div>)
 }
 
 export default TagMap;
