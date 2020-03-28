89e3f244a131bd6847eddfbb96cdd1ded69fa8c1
vl <vlyanky17@gmail.com>
2020-03-28 22:00:09 +0300

    make it better

--- client/src/pages/ToLikeSearchTag.js
+++ client/src/pages/ToLikeSearchTag.js
@@ -8,22 +8,10 @@ import logo from './lk.png';
 
 export const ToLikeSearchTag = () => {
     const {loading, error,request} = useHttp()
-    const storageName = 'userData'
-    const data = JSON.parse(localStorage.getItem(storageName))
-    const us = data.logn
-    const  d =data.ItId
-    const [lkin, setlkin] = useState({nam:us,id: d})
-    const ToLike = async () => {
-        try {
+  
 
 
-            const data = await request('/InLike', 'POST', {lkin})
-
-        } catch (e) {}
-    }
-
-
-    return (<form class="form-2"> <button  onClick={ToLike}><img src={logo} alt="logo" /> </button>{LikeCount} </form> )
+    return (11111111)
 }
 
 export default ToLikeSearchTag;
