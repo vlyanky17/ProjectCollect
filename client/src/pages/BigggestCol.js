af5216b9b6a117db241806321398fb822efa9879
vl <vlyanky17@gmail.com>
2020-03-29 13:17:45 +0300

    make it better

--- client/src/pages/BigggestCol.js
+++ client/src/pages/BigggestCol.js
@@ -52,7 +52,7 @@ export const BiggestCol = () => {
     <table class="resp-tab">
         <tbody>
         <tr >
-        <td><a  href="/ColTablSearch"> <button name={ThisCol._id} onClick={changeHandler} > открыть</button></a> </td>
+        <td><a  href="/Reg"> <button name={ThisCol._id} onClick={changeHandler} > открыть</button></a> </td>
         </tr>
         </tbody>
 
